68939a5c9c7483bc97a53d09d58d99e1dadd3966
beholder <ilovecoffeeandhatesociety@gmail.com>
2025-05-28 01:44:35 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,15 @@ echo "Cloning NixOS config from $REPO_URL to $TARGET_DIR..."
 git clone "$REPO_URL" "$TARGET_DIR"
 cd "$TARGET_DIR"
 rm -rf .git
-rm ./hosts/thinkpad/hardware-configuration.nix
-rm flake.lock
 
 echo "Generating hardware configuration..."
 nixos-generate-config --show-hardware-config > ./hosts/thinkpad/hardware-configuration.nix
 
 echo "Updating flake.nix with user: $USERNAME and hostname: $HOSTNAME..."
 
-# Заменим user и hostname в flake.nix (используем jq-like или sed, вот вариант на sed)
 sed -i -E "s/user = \"[^\"]+\";/user = \"$USERNAME\";/" flake.nix
 
 sed -i -E "s/hostname = \"[^\"]+\";/hostname = \"$HOSTNAME\";/" flake.nix || true
-# В твоём flake.nix hostname объявлен в hosts, не глобально, лучше не менять его здесь. hostname лучше передавать через nixos-rebuild (см ниже).
 
 echo "Done! Config updated with:"
 echo "  User: $USERNAME"
